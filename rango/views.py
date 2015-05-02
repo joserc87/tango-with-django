2789244ca3d1ceb8d02bc1ac741b43e2c3b3e4bc
Jose Ramon Cano <joserc87@gmail.com>
2015-05-02 23:59:01 +0200

    Chapter 11

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
@@ -16,9 +17,34 @@ def index(request):
     context_dict = {'categories': category_list,
                     'top_pages' : top_pages_list}
 
-    # Render the response and send it back!
-    return render(request, 'rango/index.html', context_dict)
 
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            # ...reassign the value of the cookie to +1 of what it was before...
+            visits = visits + 1
+            # ...and update the last visit cookie, too.
+            reset_last_visit_time = True
+    else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
+
+    response = render(request,'rango/index.html', context_dict)
+
+    return response
 def category(request, category_name_slug):
     # Create a context dictionary which we can pass to the template rendering engine.
     context_dict = {}
@@ -205,9 +231,12 @@ def user_logout(request):
     return HttpResponseRedirect('/rango/')
 
 def about(request):
-    return render(request, 'rango/about.html')
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    context_dict = {'visits': visits}
+    return render(request, 'rango/about.html', context_dict)
 
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html', {})
-    #return HttpResponse("Since you're logged in,you can see this page")
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -101,3 +101,5 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_URL = '/rango/login'
+
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
